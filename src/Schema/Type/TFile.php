c9317328f566df1f0b97724f25f06fe91bdc4982
inisire <inisire@gmail.com>
2022-12-14 15:08:11 +0200

    Remove redundant code

--- src/Schema/Type/TFile.php
+++ src/Schema/Type/TFile.php
@@ -4,12 +4,5 @@ namespace inisire\DataObject\Schema\Type;
 
 class TFile implements Type
 {
-    public function __construct(
-        private array $mimetypes
-    ) {}
-    
-    public function getMimetypes(): array
-    {
-        return $this->mimetypes;
-    }
+    public function __construct() {}
 }
