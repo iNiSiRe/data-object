0a212c4a3a29630a3f65b65c78e668d6b49ee411
inisire <inisire@gmail.com>
2023-01-11 17:47:09 +0200

    Fix TDictionary

--- src/Schema/Type/TDictionary.php
+++ src/Schema/Type/TDictionary.php
@@ -3,6 +3,8 @@
 namespace inisire\DataObject\Schema\Type;
 
 
+use inisire\DataObject\Serializer\DictionarySerializer;
+
 class TDictionary extends TPrimitive implements \inisire\DataObject\OpenAPI\Type
 {
     public function getSchema(): array
@@ -11,5 +13,10 @@ class TDictionary extends TPrimitive implements \inisire\DataObject\OpenAPI\Type
             'type' => 'object',
             'additionalProperties' => ['type' => 'string']
         ];
+    }
+
+    public function getSerializer(): string
+    {
+        return DictionarySerializer::class;
     }
 }
