543daad3105d23003598c9f620eecbb8b5ab30fe
jiumon13 <jiumon13@ukr.net>
2025-03-03 16:34:30 +0200

    Update ErrorInterface.php

--- src/Error/ErrorInterface.php
+++ src/Error/ErrorInterface.php
@@ -6,6 +6,6 @@ namespace inisire\DataObject\Error;
 
 interface ErrorInterface
 {
-    public function getCode(): string;
+    public function getCode(): ?string;
     public function getMessage(): ErrorMessage;
 }
