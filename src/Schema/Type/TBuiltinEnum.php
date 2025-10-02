3e241338d7fd8344dfb9ba140854bfdebea5c572
Hlib Kuznetsov <hlib.kuznetsov@gmail.com>
2025-10-02 08:09:21 +0000

    Add BackedEnum support

--- src/Schema/Type/TBuiltinEnum.php
+++ src/Schema/Type/TBuiltinEnum.php
@@ -2,17 +2,49 @@
 
 namespace inisire\DataObject\Schema\Type;
 
-// TODO
-class TBuiltinEnum
+class TBuiltinEnum extends TPrimitive
 {
+    private readonly TString|TInteger $type;
+    /**
+     * @var array<int|string>
+     */
+    private readonly array $options;
+
+    /**
+     * @param class-string<\BackedEnum> $enum
+     */
     public function __construct(
-        private string $enum
-    )
-    {
+        private readonly string $enum,
+    ) {
+        $reflection = new \ReflectionEnum($enum);
+
+        $this->type = match ($reflection->getBackingType()?->getName()) {
+            'string' => new TString(),
+            'int' => new TInteger(),
+            default => throw new \InvalidArgumentException(sprintf('Unsupported enum %s backing type', $enum)),
+        };
+
+        $this->options = array_map(
+            fn (\BackedEnum $case): int|string => $case->value,
+            $enum::cases(),
+        );
     }
 
     public function getEnum(): string
     {
         return $this->enum;
+    }
+
+    public function getType(): TString|TInteger
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return array<int|string>
+     */
+    public function getOptions(): array
+    {
+        return $this->options;
     }
 }
